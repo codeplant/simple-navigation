a2bb7cd127092842b4bea7d56b3020aa2237417c
Andreas Schacke <andreas.schacke@gmail.com>
2009-03-24 22:41:59 +0100

    show README upon install

--- install.rb
+++ install.rb
@@ -10,6 +10,7 @@ end
 
 begin
   copy_config_file
+  puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 rescue Exception => e
   puts "The following error ocurred while installing the plugin: #{e.message}"
 end
