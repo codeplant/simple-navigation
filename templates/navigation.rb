fe84360c66a1b3d53ef1cdf7105de397c7664510
Andreas Schacke <andreas.schacke@gmail.com>
2009-03-24 21:59:25 +0100

    + config-template

---
+++ templates/navigation.rb
@@ -0,0 +1,34 @@
+# Configures your navigation
+SimpleNavigation::Configuration.run do |navigation|  
+  # Specify a custom renderer if needed. 
+  # The default renderer is SimpleNavigation::Renderer::List which renders HTML lists.
+  # navigation.renderer = Your::Custom::Renderer
+  
+  # Specify the class that will be applied to active navigation items. Defaults to 'selected'
+  # navigation.selected_class = 'your_selected_class'
+  
+  # Define the primary navigation
+  navigation.items do |primary|
+    # Set a dom_id for this navigation if needed. The default renderer applies it to the generated <ul> tag. 
+    # primary.dom_id = 'main_navigation'
+    
+    # Add an item to the primary navigation. The following params apply:
+    # key - a symbol which uniquely defines your navigation item in the scope of the primary_navigation
+    # name - will be displayed in the rendered navigation. This can also be a call to your I18n-framework.
+    # url - the address that the generated item links to. You can also use url_helpers (named routes, restful routes helper, url_for etc.)
+    # html_options - can be used to specify attributes that will be included in the rendered navigation item (e.g. id, class etc.)
+    #
+    primary.item :key_1, 'name', url, html_options
+    
+    # Add an item which has a sub navigation (same params, but with block)
+    primary.item :key_2, 'name', url, html_options do |sub_nav|
+      # Again, dom_id can be set if needed
+      # sub_nav.dom_id = 'sub_nav'
+      
+      # Add an item to the sub navigation (same params again)
+      sub_nav.item :key_2_1, 'name', url, html_options
+    end 
+  
+  end
+  
+end
