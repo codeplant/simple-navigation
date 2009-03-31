6cdb20fc335eadb9d920472434ce941e47bdaef2
Andreas Schacke <andreas.schacke@gmail.com>
2009-03-31 19:19:39 +0200

    cleaned up install.rb and uninstall.rb

--- install.rb
+++ install.rb
@@ -1,7 +1,3 @@
-# Install hook code here
-# TODO create config/navigation.rb from template
-# TODO show README in console
-
 def copy_config_file
   config_file_template = File.join(File.dirname(__FILE__), 'templates', 'navigation.rb')
   rails_config_file = File.join(RAILS_ROOT, 'config', 'navigation.rb')
--- uninstall.rb
+++ uninstall.rb
@@ -1,2 +1,1 @@
 # Uninstall hook code here
-# TODO
