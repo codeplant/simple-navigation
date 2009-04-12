e32460cdd544bc83cf9e04c728ae8d5c56833daf
Andreas Schacke <andreas.schacke@gmail.com>
2009-04-12 23:18:33 +0200

    do not copy template upon plugin install (use generator instead)

--- install.rb
+++ install.rb
@@ -1,11 +1,4 @@
-def copy_config_file
-  config_file_template = File.join(File.dirname(__FILE__), 'templates', 'navigation.rb')
-  rails_config_file = File.join(RAILS_ROOT, 'config', 'navigation.rb')
-  FileUtils.cp config_file_template, rails_config_file, :verbose => true unless File.exist?(rails_config_file)
-end
-
 begin
-  copy_config_file
   puts IO.read(File.join(File.dirname(__FILE__), 'README'))
 rescue Exception => e
   puts "The following error ocurred while installing the plugin: #{e.message}"
