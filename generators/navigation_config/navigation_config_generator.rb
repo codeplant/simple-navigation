62d9d0d3f3f4fa13b6cb2fb40d280499ed824641
Andreas Schacke <andreas.schacke@gmail.com>
2009-04-12 22:31:54 +0200

    show README in generator

--- generators/navigation_config/navigation_config_generator.rb
+++ generators/navigation_config/navigation_config_generator.rb
@@ -2,6 +2,7 @@ class NavigationConfigGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.file "config/navigation.rb", "config/navigation.rb"
+      m.readme File.join(__FILE__, "../../README")
     end
   end
 end
