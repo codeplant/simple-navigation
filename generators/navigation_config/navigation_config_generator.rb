f5573c05037d47d46d0b9329e1a444713970d8e6
Andreas Schacke <andreas.schacke@gmail.com>
2009-04-12 22:36:38 +0200

    !pathfix

--- generators/navigation_config/navigation_config_generator.rb
+++ generators/navigation_config/navigation_config_generator.rb
@@ -2,7 +2,7 @@ class NavigationConfigGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.file "config/navigation.rb", "config/navigation.rb"
-      m.readme File.join(File.dirname(__FILE__), "../../README")
+      m.readme "../../README"
     end
   end
 end
