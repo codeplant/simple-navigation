f90d21dc3ae1c0d8a4ad68c856d16d7590b3de6c
Andreas Schacke <andreas.schacke@gmail.com>
2010-04-13 10:34:55 +0200

    . moved rendering-helper-methods to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,3 +20,46 @@ SimpleNavigation.rails_root = './'
 # Spec::Runner.configure do |config|
   # no special config
 # endx
+
+# spec helper methods
+def sub_items
+  [
+    [:subnav1, 'subnav1', 'subnav1_url', {}],
+    [:subnav2, 'subnav2', 'subnav2_url', {}]
+  ]
+end
+
+def primary_items
+  [
+    [:users, 'users', 'first_url', {:id => 'my_id'}],
+    [:invoices, 'invoices', 'second_url', {}],
+    [:accounts, 'accounts', 'third_url', {:style => 'float:right'}]
+  ]
+end
+
+def primary_container
+  container = SimpleNavigation::ItemContainer.new(0)
+  container.dom_id = 'nav_dom_id'
+  container.dom_class = 'nav_dom_class'
+  @items = primary_items.map {|params| SimpleNavigation::Item.new(container, *params)}
+  @items.each {|i| i.stub!(:selected? => false)}
+  container.instance_variable_set(:@items, @items)
+  primary_item(:invoices) {|item| item.instance_variable_set(:@sub_navigation, subnav_container)}
+  container
+end
+
+def primary_item(key)
+  yield @items.find {|i| i.key == key}
+end
+
+def select_item(key)
+  primary_item(key) {|item| item.stub!(:selected? => true)}
+end
+
+def subnav_container
+  container = SimpleNavigation::ItemContainer.new(1)
+  items = sub_items.map {|params| SimpleNavigation::Item.new(container, *params)}
+  items.each {|i| i.stub!(:selected? => false)}
+  container.instance_variable_set(:@items, items)
+  container
+end
