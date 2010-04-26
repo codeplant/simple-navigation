2837eb47d17be9aef30ba010d067d6893db550b1
Markus Schirp <mbj@seonic.net>
2010-04-26 23:07:21 +0800

    removed depreciated railtie_name from simple_navigation/railtie

--- lib/simple_navigation/railtie.rb
+++ lib/simple_navigation/railtie.rb
@@ -1,7 +1,5 @@
 module SimpleNavigation                                                                                                
   class Railtie < Rails::Railtie                                                                                       
-    railtie_name :simple_navigation                                                                                    
-                                                                                                                       
     initializer "simple_navigation.init_rails" do |app|                                                                
       SimpleNavigation::Initializer::Rails3.run
     end                                                                                                                
