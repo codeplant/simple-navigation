ed5bdd9a8949ec4d357d3c5dfc95c4409affa27b
Markus Schirp <mbj@seonic.net>
2010-06-07 01:22:35 +0800

    added breadcrumbs renderer with specs (they are mostly complete)

---
+++ lib/simple_navigation/renderer/breadcrumbs.rb
@@ -0,0 +1,38 @@
+module SimpleNavigation
+  module Renderer
+    
+    # Renders an ItemContainer as a <div> element and its containing items as <a> elements. 
+    # It onlay renders 'selected' elements.
+    #
+    # By default, the renderer sets the item's key as dom_id for the rendered <a> element unless the config option <tt>autogenerate_item_ids</tt> is set to false.
+    # The id can also be explicitely specified by setting the id in the html-options of the 'item' method in the config/navigation.rb file.
+    # The ItemContainer's dom_class and dom_id are applied to the surrounding <div> element.
+    #
+    class Breadcrumbs < SimpleNavigation::Renderer::Base
+      
+      def render(item_container)
+        a_tags = a_tags(item_container)
+        content_tag(:div, html_safe(a_tags(item_container).join(join_with)), {:id => item_container.dom_id, :class => item_container.dom_class})
+      end
+
+      protected
+
+      def a_tags(item_container)
+        item_container.items.inject([]) do |list, item|
+          if item.selected?
+            list << link_to(html_safe(item.name), item.url, {:method => item.method}.merge(item.html_options)) if item.selected?
+            if include_sub_navigation?(item)
+              list.concat a_tags(item.sub_navigation) 
+            end
+          end
+          list
+        end
+      end
+
+      def join_with
+        @join_with ||= options[:join_with] || " "
+      end
+    end
+    
+  end
+end
---
+++ spec/lib/simple_navigation/renderer/breadcrumbs_spec.rb
@@ -0,0 +1,89 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+require 'html/document' unless defined? HTML::Document
+
+describe SimpleNavigation::Renderer::Breadcrumbs do
+    
+  describe 'render' do
+    
+    def render(current_nav=nil, options={:level => :all})
+      primary_navigation = primary_container
+      select_item(current_nav) if current_nav
+      @renderer = SimpleNavigation::Renderer::Breadcrumbs.new(options)
+      HTML::Document.new(@renderer.render(primary_navigation)).root
+    end
+      
+    context 'regarding result' do
+      
+      it "should render a div-tag around the items" do
+        HTML::Selector.new('div').select(render).should have(1).entries
+      end
+      it "the rendered div-tag should have the specified dom_id" do
+        HTML::Selector.new('div#nav_dom_id').select(render).should have(1).entries
+      end
+      it "the rendered div-tag should have the specified class" do
+        HTML::Selector.new('div.nav_dom_class').select(render).should have(1).entries
+      end
+
+      context 'without current_navigation set' do
+        it "should not render any a-tag in the div-tag" do
+          HTML::Selector.new('div a').select(render).should have(0).entries
+        end
+      end
+    
+      context 'with current_navigation set' do
+        it "should render the selected a tags" do
+          HTML::Selector.new('div a').select(render(:invoices)).should have(1).entries
+        end
+      end
+    
+    
+      context 'nested sub_navigation' do
+        it "should add an a tag for each selected item" do
+          HTML::Selector.new('div a').select(render(:subnav1)).should have(2).entries
+        end
+      end
+    end
+    
+    context 'regarding method calls' do
+      
+      context 'regarding the list_content' do
+        before(:each) do
+          @primary_navigation = primary_container
+          @list_content = stub(:list_content)
+          @list_items = stub(:list_items, :join => @list_content)
+          @items.stub!(:inject => @list_items)
+          @renderer = SimpleNavigation::Renderer::Breadcrumbs.new(options)
+        end
+      
+        it "should join the list_items" do
+          @list_items.should_receive(:join)
+        end
+      
+        it "should html_saferize the list_content" do
+          @renderer.should_receive(:html_safe).with(@list_content)
+        end
+
+        after(:each) do
+          @renderer.render(@primary_navigation)
+        end
+      end
+      
+      context 'regarding the items' do
+        before(:each) do
+          @primary_navigation = primary_container
+          select_item(:invoices)
+          @renderer = SimpleNavigation::Renderer::Breadcrumbs.new(options)
+        end
+        
+        it "should call html_safe on every rendered item's name" do
+          @items.each do |item|
+            @renderer.should_receive(:html_safe).with(item.name) if item.selected?
+          end
+          @renderer.should_receive(:html_safe).with(anything).twice()
+          @renderer.render(@primary_navigation)
+        end
+      end
+      
+    end
+  end
+end
