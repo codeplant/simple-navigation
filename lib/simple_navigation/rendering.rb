27f30dbb1013bb857f2e8d7710ae7b0d9a4780c8
Andreas Schacke <andreas.schacke@gmail.com>
2010-08-30 13:45:06 +0200

    + added autoloading for renderers

--- lib/simple_navigation/rendering.rb
+++ lib/simple_navigation/rendering.rb
@@ -1,5 +1,10 @@
 require 'simple_navigation/rendering/helpers'
 require 'simple_navigation/rendering/renderer/base'
-require 'simple_navigation/rendering/renderer/list'
-require 'simple_navigation/rendering/renderer/links'
-require 'simple_navigation/rendering/renderer/breadcrumbs'
+
+module SimpleNavigation
+  module Renderer
+    autoload :List, 'simple_navigation/rendering/renderer/list'
+    autoload :Links, 'simple_navigation/rendering/renderer/links'
+    autoload :Breadcrumbs, 'simple_navigation/rendering/renderer/breadcrumbs'
+  end
+end
