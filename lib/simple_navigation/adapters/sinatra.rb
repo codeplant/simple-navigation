2ee546676f66b1cc04b0523de0fdac5307cd6aef
Andreas Schacke <andreas.schacke@gmail.com>
2010-09-01 12:27:10 +0200

    . finished sinatra adapter

--- lib/simple_navigation/adapters/sinatra.rb
+++ lib/simple_navigation/adapters/sinatra.rb
@@ -1,15 +1,76 @@
 module SimpleNavigation
   module Adapters
-    class Sinatra
+    class Sinatra < Base
       
-      def self.init_framework
+      def self.register
+        SimpleNavigation.set_env(sinatra_root, sinatra_environment)
+        ::Sinatra::Application.send(:helpers, SimpleNavigation::Helpers)
       end
       
       def initialize(context)
-        puts "XXXXX #{context.request}"
+        @context = context
+        @request = context.request
+      end
+
+      def context_for_eval
+        raise 'no context set for evaluation the config file' unless context
+        context
       end
       
+      def request_uri
+        request.fullpath
+      end
+      
+      def request_path
+        request.path
+      end
+      
+      def current_page?(url)
+        url_string = CGI.unescape(url)
+        if url_string.index("?")
+          uri = request_uri
+        else
+          uri = request_uri.split('?').first
+        end
+        if url_string =~ /^\w+:\/\//
+          url_string == "#{request.protocol}#{request.host_with_port}#{uri}"
+        else
+          url_string == uri
+        end        
+      end
+      
+      def link_to(name, url, options={})
+        "<a href='#{url}' #{to_attributes(options)}>#{name}</a>"
+      end
+      
+      def content_tag(type, content, options={})
+        "<#{type} #{to_attributes(options)}>#{content}</#{type}>"
+      end
+      
+      protected
+      
+      def self.sinatra_root
+        ::Sinatra::Application.root
+      end
+      
+      def self.sinatra_environment
+        ::Sinatra::Application.environment
+      end
+      
+      def to_attributes(options)
+        options.map {|k, v| "#{k}='#{v}'"}.join(' ')
+      end
       
     end
   end
+end
+
+module SimpleNavigation
+  
+  # Adds registered method to SimpleNavigation. This is called when executing 'register SimpleNavigation'.
+  def self.registered(app)
+    register
+    app.helpers Helpers
+  end
+  
 end
---
+++ spec/lib/simple_navigation/adapters/sinatra_spec.rb
@@ -0,0 +1,60 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+
+describe SimpleNavigation::Adapters::Sinatra do
+
+  def create_adapter
+    SimpleNavigation::Adapters::Sinatra.new(@context)
+  end
+
+  before(:each) do
+    @context = stub(:context)
+    @request = stub(:request, :fullpath => '/full?param=true', :path => '/full')
+    @context.stub!(:request => @request)
+    @adapter = create_adapter
+  end
+
+  describe 'context_for_eval' do
+    it "should raise error if no context" do
+      @adapter.stub!(:context => nil)
+      lambda {@adapter.context_for_eval}.should raise_error
+    end
+    it "should return the context" do
+      @adapter.context_for_eval.should == @context
+    end
+  end
+  
+  describe 'request_uri' do
+    it {@adapter.request_uri.should == '/full?param=true'}
+  end
+  
+  describe 'request_path' do
+    it {@adapter.request_path.should == '/full'}
+  end
+  
+  describe 'current_page?' do
+    before(:each) do
+      @request.stub!(:protocol => 'http://', :host_with_port => 'my_host:5000')
+    end
+    it {@adapter.current_page?('/full?param=true').should be_true}
+    it {@adapter.current_page?('/full?param3=true').should be_false}
+    it {@adapter.current_page?('/full').should be_true}
+    it {@adapter.current_page?('http://my_host:5000/full?param=true').should be_true}
+    it {@adapter.current_page?('http://my_host:5000/full?param3=true').should be_false}
+    it {@adapter.current_page?('http://my_host:5000/full').should be_true}
+    it {@adapter.current_page?('http://my_host:6000/full').should be_false}
+    it {@adapter.current_page?('http://my_other_host:5000/full').should be_false}
+  end
+  
+  describe 'link_to' do
+    it "should return a link" do
+      @adapter.link_to('link', 'url', :class => 'clazz', :id => 'id').should == "<a href='url' class='clazz' id='id'>link</a>"
+    end
+  end
+  
+  describe 'content_tag' do
+    it "should return a tag" do
+      @adapter.content_tag(:div, 'content', :class => 'clazz', :id => 'id').should == "<div class='clazz' id='id'>content</div>"
+    end
+  end
+  
+end
