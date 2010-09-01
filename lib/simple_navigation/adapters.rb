b4b9c6b546b46a32d5f90c2659c9bb87f094a83f
Andreas Schacke <andreas.schacke@gmail.com>
2010-09-01 12:27:30 +0200

    . requiring base adapter

--- lib/simple_navigation/adapters.rb
+++ lib/simple_navigation/adapters.rb
@@ -1,6 +1,9 @@
+require 'simple_navigation/adapters/base'
+
 module SimpleNavigation
   module Adapters
     autoload :Rails, 'simple_navigation/adapters/rails'
+    autoload :Padrino, 'simple_navigation/adapters/padrino'
     autoload :Sinatra, 'simple_navigation/adapters/sinatra'
   end
 end
