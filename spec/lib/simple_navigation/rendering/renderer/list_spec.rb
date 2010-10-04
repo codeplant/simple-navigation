076a985fc3831666ed98588c0c5942f58a2a26cd
Jack Dempsey <jack.dempsey@gmail.com>
2010-10-04 19:01:45 -0400

    require just the spec_helper so absolute paths in 1.9 aren't borked

--- spec/lib/simple_navigation/rendering/renderer/list_spec.rb
+++ spec/lib/simple_navigation/rendering/renderer/list_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../../../spec_helper'
+require 'spec_helper'
 require 'html/document' unless defined? HTML::Document
 
 describe SimpleNavigation::Renderer::List do
