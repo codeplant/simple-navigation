023c25f08b7e325a62c628ae8687b7c208954ad0
Simon Courtois <scourtois+github@cubyx.fr>
2014-02-15 13:00:40 +0100

    Cleaning up the Sinatra adapter spec

--- spec/lib/simple_navigation/adapters/sinatra_spec.rb
+++ spec/lib/simple_navigation/adapters/sinatra_spec.rb
@@ -1,80 +1,91 @@
 require 'spec_helper'
 
 describe SimpleNavigation::Adapters::Sinatra do
+  let(:adapter) { SimpleNavigation::Adapters::Sinatra.new(context) }
+  let(:context) { double(:context) }
+  let(:request) { double(:request, fullpath: '/full?param=true', path: '/full') }
 
-  def create_adapter
-    SimpleNavigation::Adapters::Sinatra.new(@context)
-  end
+  before { context.stub(request: request) }
 
-  before(:each) do
-    @context = double(:context)
-    @request = double(:request, :fullpath => '/full?param=true', :path => '/full')
-    @context.stub(:request => @request)
-    @adapter = create_adapter
-  end
-
-  describe 'context_for_eval' do
-    it "should raise error if no context" do
-      @adapter.stub(:context => nil)
-      lambda {@adapter.context_for_eval}.should raise_error
-    end
-    it "should return the context" do
-      @adapter.context_for_eval.should == @context
-    end
-  end
-
-  describe 'request_uri' do
-    it {@adapter.request_uri.should == '/full?param=true'}
-  end
-
-  describe 'request_path' do
-    it {@adapter.request_path.should == '/full'}
-  end
-
-  describe 'current_page?' do
-    before(:each) do
-      @request.stub(:scheme => 'http', :host_with_port => 'my_host:5000')
+  describe '#context_for_eval' do
+    context "when adapter's context is not set" do
+      it 'raises an exception' do
+        adapter.stub(context: nil)
+        expect{ adapter.context_for_eval }.to raise_error
+      end
     end
 
-    describe 'when URL is not encoded' do
-      it {@adapter.current_page?('/full?param=true').should be_true}
-      it {@adapter.current_page?('/full?param3=true').should be_false}
-      it {@adapter.current_page?('/full').should be_true}
-      it {@adapter.current_page?('http://my_host:5000/full?param=true').should be_true}
-      it {@adapter.current_page?('http://my_host:5000/full?param3=true').should be_false}
-      it {@adapter.current_page?('http://my_host:5000/full').should be_true}
-      it {@adapter.current_page?('https://my_host:5000/full').should be_false}
-      it {@adapter.current_page?('http://my_host:6000/full').should be_false}
-      it {@adapter.current_page?('http://my_other_host:5000/full').should be_false}
+    context "when adapter's context is set" do
+      it 'returns the context' do
+        expect(adapter.context_for_eval).to be context
+      end
+    end
+  end
+
+  describe '#request_uri' do
+    it 'returns the request.fullpath' do
+      expect(adapter.request_uri).to eq '/full?param=true'
+    end
+  end
+
+  describe '#request_path' do
+    it 'returns the request.path' do
+      expect(adapter.request_path).to eq '/full'
+    end
+  end
+
+  describe '#current_page?' do
+    before { request.stub(scheme: 'http', host_with_port: 'my_host:5000') }
+
+    shared_examples 'detecting current page' do |url, expected|
+      context "when url is #{url}" do
+        it "returns #{expected}" do
+          expect(adapter.current_page?(url)).to be expected
+        end
+      end
     end
 
-    describe 'when URL is encoded' do
-      before(:each) do
-        @request.stub(:fullpath => '/full%20with%20spaces?param=true', :path => '/full%20with%20spaces')
+    context 'when URL is not encoded' do
+      it_behaves_like 'detecting current page', '/full?param=true', true
+      it_behaves_like 'detecting current page', '/full?param3=true', false
+      it_behaves_like 'detecting current page', '/full', true
+      it_behaves_like 'detecting current page', 'http://my_host:5000/full?param=true', true
+      it_behaves_like 'detecting current page', 'http://my_host:5000/full?param3=true', false
+      it_behaves_like 'detecting current page', 'http://my_host:5000/full', true
+      it_behaves_like 'detecting current page', 'https://my_host:5000/full', false
+      it_behaves_like 'detecting current page', 'http://my_host:6000/full', false
+      it_behaves_like 'detecting current page', 'http://my_other_host:5000/full', false
+    end
+
+    context 'when URL is encoded' do
+      before do
+        request.stub(fullpath: '/full%20with%20spaces?param=true',
+                     path: '/full%20with%20spaces')
       end
 
-      it {@adapter.current_page?('/full%20with%20spaces?param=true').should be_true}
-      it {@adapter.current_page?('/full%20with%20spaces?param3=true').should be_false}
-      it {@adapter.current_page?('/full%20with%20spaces').should be_true}
-      it {@adapter.current_page?('http://my_host:5000/full%20with%20spaces?param=true').should be_true}
-      it {@adapter.current_page?('http://my_host:5000/full%20with%20spaces?param3=true').should be_false}
-      it {@adapter.current_page?('http://my_host:5000/full%20with%20spaces').should be_true}
-      it {@adapter.current_page?('https://my_host:5000/full%20with%20spaces').should be_false}
-      it {@adapter.current_page?('http://my_host:6000/full%20with%20spaces').should be_false}
-      it {@adapter.current_page?('http://my_other_host:5000/full%20with%20spaces').should be_false}
+      it_behaves_like 'detecting current page', '/full%20with%20spaces?param=true', true
+      it_behaves_like 'detecting current page', '/full%20with%20spaces?param3=true', false
+      it_behaves_like 'detecting current page', '/full%20with%20spaces', true
+      it_behaves_like 'detecting current page', 'http://my_host:5000/full%20with%20spaces?param=true', true
+      it_behaves_like 'detecting current page', 'http://my_host:5000/full%20with%20spaces?param3=true', false
+      it_behaves_like 'detecting current page', 'http://my_host:5000/full%20with%20spaces', true
+      it_behaves_like 'detecting current page', 'https://my_host:5000/full%20with%20spaces', false
+      it_behaves_like 'detecting current page', 'http://my_host:6000/full%20with%20spaces', false
+      it_behaves_like 'detecting current page', 'http://my_other_host:5000/full%20with%20spaces', false
     end
   end
 
-  describe 'link_to' do
-    it "should return a link" do
-      @adapter.link_to('link', 'url', :class => 'clazz', :id => 'id').should == "<a href='url' class='clazz' id='id'>link</a>"
+  describe '#link_to' do
+    it 'returns a link with the correct class and id' do
+      link = adapter.link_to('link', 'url', class: 'clazz', id: 'id')
+      expect(link).to eq "<a href='url' class='clazz' id='id'>link</a>"
     end
   end
 
-  describe 'content_tag' do
-    it "should return a tag" do
-      @adapter.content_tag(:div, 'content', :class => 'clazz', :id => 'id').should == "<div class='clazz' id='id'>content</div>"
+  describe '#content_tag' do
+    it 'returns a tag with the correct class and id' do
+      tag = adapter.content_tag(:div, 'content', class: 'clazz', id: 'id')
+      expect(tag).to eq "<div class='clazz' id='id'>content</div>"
     end
   end
-
 end
