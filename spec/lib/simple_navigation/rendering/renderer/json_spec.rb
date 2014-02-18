18f2bfdb9f93e0d80a604a69a136faff39e8b834
Simon Courtois <scourtois+github@cubyx.fr>
2014-02-18 10:43:11 +0100

    Cleaning up the Json renderer spec

--- spec/lib/simple_navigation/rendering/renderer/json_spec.rb
+++ spec/lib/simple_navigation/rendering/renderer/json_spec.rb
@@ -1,48 +1,69 @@
 require 'spec_helper'
+require 'json_spec'
 
-describe SimpleNavigation::Renderer::Json do
+RSpec.configure { |config| config.include JsonSpec::Helpers }
 
-  describe 'render' do
+module SimpleNavigation
+  module Renderer
+    describe Json do
+      describe '#render' do
+        let!(:navigation) { setup_navigation('nav_id', 'nav_class') }
 
-    def render(current_nav=nil, options={})
-      primary_navigation = primary_container
-      select_item(current_nav)
-      setup_renderer_for SimpleNavigation::Renderer::Json, :rails, options
-      @renderer.render(primary_navigation)
-    end
+        let(:item) { :invoices }
+        let(:options) {{ level: :all }}
+        let(:output) { renderer.render(navigation) }
+        let(:parsed_output) { parse_json(output) }
+        let(:renderer) { setup_renderer(Json, options) }
 
-    def prerendered_menu
-      '[{"name":"users","url":"first_url","selected":false,"items":null},{"name":"invoices","url":"second_url","selected":true,"items":[{"name":"subnav1","url":"subnav1_url","selected":false,"items":null},{"name":"subnav2","url":"subnav2_url","selected":false,"items":null}]},{"name":"accounts","url":"third_url","selected":false,"items":null},{"name":"miscellany","url":null,"selected":false,"items":null}]'
-    end
+        before { select_an_item(navigation[item]) if item }
 
-    context 'regarding result' do
+        context 'when an item is selected' do
 
-      it "should return a string" do
-        render(:invoices).class.should == String
-      end
-
-      it "should render the selected page" do
-        json = parse_json(render(:invoices))
-        found = json.any? do |item| 
-          item["name"] == "invoices" and item["selected"]
+          it 'renders the selected page' do
+            invoices_item = parsed_output.find { |item| item['name'] == 'Invoices' }
+            expect(invoices_item).to include('selected' => true)
+          end
         end
-        found.should == true
-      end
 
-    end
+        # FIXME: not sure if :as_hash returning an array makes sense...
+        context 'when the :as_hash option is true' do
+          let(:options) {{ level: :all, as_hash: true }}
 
-    context 'regarding hash result' do
-      it "should return a hash" do
-        render(:invoices, :as_hash => true).class.should == Array
-      end
+          it 'returns a hash' do
+            expect(output).to be_an Array
+          end
 
-      it "should render the selected page" do
-        found = render(:invoices, :as_hash => true).any? do |item|
-          item[:name] == "invoices" and item[:selected]
+          it 'renders the selected page' do
+            invoices_item = output.find { |item| item[:name] == 'Invoices' }
+            expect(invoices_item).to include(selected: true)
+          end
         end
-        found.should == true
-      end
 
+        context 'when a sub navigation item is selected' do
+          let(:invoices_item) do
+            parsed_output.find { |item| item['name'] == 'Invoices' }
+          end
+          let(:unpaid_item) do
+            invoices_item['items'].find { |item| item['name'] == 'Unpaid' }
+          end
+
+          before do
+            navigation[:invoices].stub(selected?: true)
+
+            navigation[:invoices]
+              .sub_navigation[:unpaid]
+              .stub(selected?: true, selected_by_condition?: true)
+          end
+
+          it 'marks all the parent items as selected' do
+            expect(invoices_item).to include('selected' => true)
+          end
+
+          it 'marks the item as selected' do
+            expect(unpaid_item).to include('selected' => true)
+          end
+        end
+      end
     end
   end
 end
