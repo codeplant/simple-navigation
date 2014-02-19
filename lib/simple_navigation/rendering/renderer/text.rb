7cde6ac9064597d133a459e3a22e1635073262e4
Simon Courtois <scourtois+github@cubyx.fr>
2014-02-19 22:47:24 +0100

    Cleaning up the Text renderer code

--- lib/simple_navigation/rendering/renderer/text.rb
+++ lib/simple_navigation/rendering/renderer/text.rb
@@ -1,26 +1,21 @@
 module SimpleNavigation
   module Renderer
-    
-    # Renders the 'chain' of selected navigation items as simple text items, joined with an optional separator (similar to breadcrumbs, but without markup).
-    #
+    # Renders the 'chain' of selected navigation items as simple text items,
+    # joined with an optional separator (similar to breadcrumbs, but without
+    # markup).
     class Text < SimpleNavigation::Renderer::Base
-      
       def render(item_container)
-        list(item_container).compact.join(options[:join_with] || " ")
+        list(item_container).compact.join(options[:join_with] || ' ')
       end
 
       private
 
       def list(item_container)
-        item_container.items.inject([]) do |array, item|
-          if item.selected?
-            array + [item.name(:apply_generator => false)] + (include_sub_navigation?(item) ? list(item.sub_navigation) : [])
-          else
-            array
-          end
+        item_container.items.keep_if(&:selected?).map do |item|
+          [item.name(apply_generator: false)] +
+          (include_sub_navigation?(item) ? list(item.sub_navigation) : [])
         end
       end
-
     end
   end
 end
