814084043ee1568d122f37e930dc9d4b926a2d55
Andreas Schacke <andreas.schacke@gmail.com>
2014-02-26 20:28:04 +0100

    ! #151 fixing generator

--- generators/navigation_config/navigation_config_generator.rb
+++ generators/navigation_config/navigation_config_generator.rb
@@ -2,7 +2,7 @@ class NavigationConfigGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.file 'config/navigation.rb', 'config/navigation.rb'
-      m.readme '../../../README'
+      m.readme '../../../README.md'
     end
   end
 end
