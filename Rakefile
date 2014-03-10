codeplant/simple-navigation

commit 2cf2c2f5bd642ce547be5531befd72d58c74e453

Respecting namespacing in files organization

Rakefile
@@ -1,5 +1,5 @@
 require 'bundler/gem_tasks'
-require 'rspec/core/rake_task'
+require 'rspec/rake_task'
 require 'rdoc/task'
 
 RSpec::Core::RakeTask.new(:spec)

lib/simple_navigation.rb
@@ -4,7 +4,11 @@ require 'active_support/core_ext/hash'
 require 'active_support/core_ext/module/attribute_accessors'
 
 require 'simple_navigation/version'
-require 'simple_navigation/core'
+require 'simple_navigation/configuration'
+require 'simple_navigation/item_adapter'
+require 'simple_navigation/item'
+require 'simple_navigation/item_container'
+require 'simple_navigation/items_provider'
 require 'simple_navigation/rendering'
 require 'simple_navigation/adapters'
 require 'simple_navigation/config_file_finder'

lib/simple_navigation/core.rb
@@ -1,5 +1,0 @@
-require 'simple_navigation/core/configuration'
-require 'simple_navigation/core/item_adapter'
-require 'simple_navigation/core/item'
-require 'simple_navigation/core/item_container'
-require 'simple_navigation/core/items_provider'

commit cf7112b2fd675181751c8c0ad8fc4cb14df41c20

Fixing a Rakefile typo introduced in 2cf2c2f5

Rakefile
@@ -1,5 +1,5 @@
 require 'bundler/gem_tasks'
-require 'rspec/rake_task'
+require 'rspec/core/rake_task'
 require 'rdoc/task'
 
 RSpec::Core::RakeTask.new(:spec)