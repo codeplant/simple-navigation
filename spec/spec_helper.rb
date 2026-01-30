4e0206a74736cad7c8533680e71b421c73163585
Nicolas Rodriguez <nico@nicoladmin.fr>
2026-01-30 03:10:42 +0100

    Minor change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -88,27 +88,6 @@ module SimpleNavigationTest
   end
 end
 
-# Define our own matcher
-RSpec::Matchers.define :have_css do |expected, times|
-  match do |actual|
-    selector = Nokogiri::HTML(actual).css(expected)
-
-    if times
-      expect(selector.size).to eq times
-    else
-      expect(selector.size).to be >= 1
-    end
-  end
-
-  failure_message do |actual|
-    "expected #{actual} to have #{times || 1} elements matching '#{expected}'"
-  end
-
-  failure_message_when_negated do |actual|
-    "expected #{actual} not to have #{times || 1} elements matching '#{expected}'"
-  end
-end
-
 # Configure RSpec
 RSpec.configure do |config|
   config.include SimpleNavigationTest
@@ -131,6 +110,27 @@ RSpec.configure do |config|
 
   config.around(memfs: true) do |example|
     MemFs.activate { example.run }
+  end
+end
+
+# Define our own matcher
+RSpec::Matchers.define :have_css do |expected, times|
+  match do |actual|
+    selector = Nokogiri::HTML(actual).css(expected)
+
+    if times
+      expect(selector.size).to eq times
+    else
+      expect(selector.size).to be >= 1
+    end
+  end
+
+  failure_message do |actual|
+    "expected #{actual} to have #{times || 1} elements matching '#{expected}'"
+  end
+
+  failure_message_when_negated do |actual|
+    "expected #{actual} not to have #{times || 1} elements matching '#{expected}'"
   end
 end
 
